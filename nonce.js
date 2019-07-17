7bf36e68d80510b28736894d6ca4ec416b3d564b
rhodey <notrhodey@protonmail.com>
2019-07-16 15:40:00 -1000

    split it up

--- nonce.js
+++ nonce.js
@@ -1,6 +1,7 @@
+const level = require('level-mem')
 const umkv = require('unordered-materialized-kv')
-let db = require('level')('./kv.db')
-let kv = umkv(db)
+const BotState = require('./state.js').BotState
+const states = require('./state.js').states
 
 let inbox = [
   { key : 'A', seq : 0, value : { content : { channel : 'bots', text : '!rollcall AA00' }}},
@@ -15,140 +16,36 @@ let inbox = [
   { key : 'B', seq : 1, value : { content : { channel : 'bots', text : '!done dat://abc777' }}},
 ]
 
-let inboxA = { key : 'A', inbox }
-let inboxB = { key : 'B', inbox : [inbox[1], inbox[2], inbox[3], inbox[4], inbox[5], inbox[6], inbox[7], inbox[8], inbox[9]] }
-let inboxC = { key : 'C', inbox : [inbox[2], inbox[3], inbox[5], inbox[6], inbox[7], inbox[8]] }
-let inboxD = { key : 'D', inbox }
-let inboxE = { key : 'E', inbox : [inbox[0], inbox[1], inbox[2], inbox[3], inbox[4]] }
+let inboxA = inbox
+let inboxB = [inbox[1], inbox[2], inbox[3], inbox[4], inbox[5], inbox[6], inbox[7], inbox[8], inbox[9]]
+let inboxC = [inbox[2], inbox[3], inbox[5], inbox[6], inbox[7], inbox[8]]
+let inboxD = inbox
+let inboxE = [inbox[0], inbox[1], inbox[2], inbox[3], inbox[4]]
 
-function SwarmState(id, inbox) {
-  const WAIT_ROLLCALL = 0
-  const ACK_ROLLCALL = 1
-  const WAIT_JOB = 2
-  const DO_JOB = 3
+let db = level()
+let kv = umkv(db)
+let state = BotState('B', db, kv)
 
-  let nonces = { }
-  let peers = { }
-  let state = WAIT_ROLLCALL
+next(0, inboxB, (err, state) => {
+  console.error(err)
+  console.log('done')
+})
 
-  function clearDb(cb) {
-    db.createKeyStream()
-      .on('data', (key) => db.del(key))
-      .once('end', cb)
-  }
+function next(idx, inbox, cb) {
+  if (idx >= inbox.length) return cb(null)
+  console.log(idx, inbox[idx])
+  state.next(inbox[idx], (err, state) => {
+    if (err) return cb(err)
+    console.log(state)
+    switch (state) {
+      case states.ACK_ROLL:
+        break;
 
-  function filter(msg) {
-    if (!msg.value) return
-    if (!msg.value.content) return
-    if (msg.value.content.channel !== 'bots') return
-    if (typeof msg.value.content.text !== 'string') return
-    return true
-  }
-
-  function followPointer(ptr, bots, cb) {
-    let val = nonces[ptr]
-    if (val === undefined) {
-      cb('message ' + ptr + ' not found.')
-    } else if (val === 0) {
-      cb(null)
-    } else {
-      let bot = val.id
-      bots.add(bot)
-      followPointers(0, val.prev.split(','), bots, cb)
+      case states.DO_JOB:
+        break;
     }
-  }
-
-  function followPointers(idx, ptrs, bots, cb) {
-    if (idx >= ptrs.length) { return cb(null, bots) }
-    let ptr = ptrs[idx]
-
-    followPointer(ptr, bots, (err) => {
-      if (err) cb(err)
-      else followPointers(idx + 1, ptrs, bots, cb)
-    })
-  }
-
-  function next(idx, cb) {
-    if (idx >= inbox.length) { return cb(null, state) }
-    let msg = inbox[idx]
-    console.log(idx, msg)
-    let text = msg.value.content.text
-
-    if (/^!rollcall\b/.test(text)) {
-      if (state === WAIT_ROLLCALL) {
-        state = ACK_ROLLCALL
-      }
-
-      let parts = text.split(' ')
-      if (parts.length != 2) return next(idx + 1, cb)
-      let nonce = parts[1]
-      nonces[nonce] = 0
-
-      clearDb(() => {
-        let doc = { id : nonce, key : 'head', links : [] }
-        kv.batch([doc], (err) => {
-          if (err) cb(err)
-          else next(idx + 1, cb)
-        })
-      })
-    } else if (/^!ok\b/.test(text)) {
-      if (state === ACK_ROLLCALL && id === msg.key) {
-        state = WAIT_JOB
-      }
-
-      let parts = text.split(' ')
-      if (parts.length != 4) return next(idx + 1, cb)
-      let nonce = parts[1]
-      let prev = parts[2]
-      let archive = parts[3]
-
-      nonces[nonce] = { id : msg.key, prev }
-      peers[msg.key] = archive
-
-      let doc = { id : nonce, key : 'head', links : prev.split(',') }
-      kv.batch([doc], (err) => {
-        if (err) cb(err)
-        else next(idx + 1, cb)
-      })
-    } else if (state === WAIT_JOB && /^!job\b/.test(text)) {
-      let parts = text.split(' ')
-      if (parts.length != 3) return next(idx + 1, cb)
-      let prev = parts[1].split(',')
-      let job = parts[2]
-
-      followPointers(0, prev, new Set(), (err, bots) => {
-        if (err) {
-          state = WAIT_ROLLCALL
-          console.error('unable to start job', job, 'because error:', err)
-        } else if (bots.has(id) >= 0) {
-          state = DO_JOB
-          let archives = Array.from(bots).map((key) => peers[key])
-          console.log('do job w/', bots, archives)
-        } else {
-          console.log('skip job w/', bots)
-        }
-        next(idx + 1, cb)
-      })
-    } else if (/^!done\b/.test(text)) {
-      if (state === DO_JOB && id === msg.key) {
-        state = WAIT_ROLLCALL
-      }
-      next(idx + 1, cb)
-    } else {
-      next(idx + 1, cb)
-    }
-  }
-
-  return { next }
+    next(idx + 1, inbox, cb)
+  })
 }
 
-SwarmState(inboxB.key, inboxB.inbox)
-  .next(0, (err, state) => {
-    console.error('error', err)
-    console.log(state)
-
-    kv.get('head', (err, ids) => {
-      console.error('error', err)
-      console.log('head ->', ids)
-    })
-  })
+console.log(states)
--- state.js
+++ state.js
@@ -1,35 +1,18 @@
-const umkv = require('unordered-materialized-kv')
-let db = require('level')('./kv.db')
-let kv = umkv(db)
+const states = {
+  WAIT_ROLL: 0,
+  ACK_ROLL : 1,
+  WAIT_JOB : 2,
+  DO_JOB : 3,
+}
 
-let inbox = [
-  { key : 'A', seq : 0, value : { content : { channel : 'bots', text : '!rollcall' }}},
-  { key : 'Z', seq : 0, value : { content : { channel : 'bots', text : '!ok A:0 dat://Z' }}},
-  { key : 'A', seq : 1, value : { content : { channel : 'bots', text : '!job Z:0 dat://abc666' }}},
-  { key : 'A', seq : 2, value : { content : { channel : 'bots', text : '!rollcall' }}},
-  { key : 'B', seq : 0, value : { content : { channel : 'bots', text : '!ok A:2 dat://B' }}},
-  { key : 'C', seq : 0, value : { content : { channel : 'bots', text : '!ok A:2 dat://C' }}},
-  { key : 'D', seq : 0, value : { content : { channel : 'bots', text : '!ok B:0,C:0 dat://D' }}},
-  { key : 'E', seq : 0, value : { content : { channel : 'bots', text : '!ok B:0 dat://E' }}},
-  { key : 'A', seq : 3, value : { content : { channel : 'bots', text : '!job E:0,D:0 dat://abc777' }}},
-  { key : 'B', seq : 1, value : { content : { channel : 'bots', text : '!done A:3' }}},
-]
+module.exports = {
+  states, BotState
+}
 
-let inboxA = { key : 'A', inbox }
-let inboxB = { key : 'B', inbox : [inbox[1], inbox[2], inbox[3], inbox[4], inbox[5], inbox[6], inbox[7], inbox[8], inbox[9]] }
-let inboxC = { key : 'C', inbox : [inbox[2], inbox[3], inbox[5], inbox[6], inbox[7], inbox[8]] }
-let inboxD = { key : 'D', inbox }
-let inboxE = { key : 'E', inbox : [inbox[0], inbox[1], inbox[2], inbox[3], inbox[4]] }
-
-function SwarmState(id, inbox) {
-  const WAIT_ROLLCALL = 0
-  const ACK_ROLLCALL = 1
-  const WAIT_JOB = 2
-  const DO_JOB = 3
-
-  let pointers = { }
-  let archives = { }
-  let state = WAIT_ROLLCALL
+function BotState(id, db, kv) {
+  let nonces = { }
+  let peers = { }
+  let state = states.WAIT_ROLL
 
   function clearDb(cb) {
     db.createKeyStream()
@@ -45,103 +28,95 @@ function SwarmState(id, inbox) {
     return true
   }
 
-  function followPtr(ptr, bots, cb) {
-    let val = pointers[ptr]
+  function followPointer(ptr, bots, cb) {
+    let val = nonces[ptr]
     if (val === undefined) {
       cb('message ' + ptr + ' not found.')
     } else if (val === 0) {
       cb(null)
     } else {
-      let bot = ptr.split(':')[0]
+      let bot = val.id
       bots.add(bot)
-      followPtrs(0, val.split(','), bots, cb)
+      followPointers(0, val.prev.split(','), bots, cb)
     }
   }
 
-  function followPtrs(idx, ptrs, bots, cb) {
+  function followPointers(idx, ptrs, bots, cb) {
     if (idx >= ptrs.length) { return cb(null, bots) }
     let ptr = ptrs[idx]
-    let bot = ptr.split(':')[0]
 
-    followPtr(ptr, bots, (err) => {
+    followPointer(ptr, bots, (err) => {
       if (err) cb(err)
-      else followPtrs(idx + 1, ptrs, bots, cb)
+      else followPointers(idx + 1, ptrs, bots, cb)
     })
   }
 
-  function next(idx, cb) {
-    if (idx >= inbox.length) { return cb(null, state) }
-
-    let msg = inbox[idx]
-    console.log(idx, msg)
+  function next(msg, cb) {
     let text = msg.value.content.text
-    if (/^!rollcall\b/.test(text)) {
-      if (state === WAIT_ROLLCALL) {
-        state = ACK_ROLLCALL
-      }
-      let mid = msg.key + ':' + msg.seq
-      pointers[mid] = 0
 
+    if (/^!rollcall\b/.test(text)) {
+      let parts = text.split(' ')
+      if (parts.length != 2) return cb(null, state)
+      if (state === states.WAIT_ROLL) {
+        state = states.ACK_ROLL
+      }
+
+      let nonce = parts[1]
+      nonces[nonce] = 0
       clearDb(() => {
-        let doc = { id : mid, key : 'head', links : [] }
+        let doc = { id : nonce, key : 'head', links : [] }
         kv.batch([doc], (err) => {
           if (err) cb(err)
-          else next(idx + 1, cb)
+          else cb(null, state)
         })
       })
     } else if (/^!ok\b/.test(text)) {
-      if (state === ACK_ROLLCALL && id === msg.key) {
-        state = WAIT_JOB
-      }
-      let mid = msg.key + ':' + msg.seq
       let parts = text.split(' ')
-      if (parts.length != 3) return next(idx + 1, cb)
+      if (parts.length != 4) return cb(null, state)
+      if (state === states.ACK_ROLL && id === msg.key) {
+        state = states.WAIT_JOB
+      }
 
-      pointers[mid] = parts[1]
-      archives[msg.key] = parts[2]
+      let nonce = parts[1]
+      let prev = parts[2]
+      let archive = parts[3]
 
-      let doc = { id : mid, key : 'head', links : parts[1].split(',') }
+      nonces[nonce] = { id : msg.key, prev }
+      peers[msg.key] = archive
+
+      let doc = { id : nonce, key : 'head', links : prev.split(',') }
       kv.batch([doc], (err) => {
         if (err) cb(err)
-        else next(idx + 1, cb)
+        else cb(null, state)
       })
-    } else if (state === WAIT_JOB && /^!job\b/.test(text)) {
+    } else if (state === states.WAIT_JOB && /^!job\b/.test(text)) {
       let parts = text.split(' ')
-      if (parts.length != 3) return next(idx + 1, cb)
-      let ptrs = parts[1].split(',')
+      if (parts.length != 3) cb(null, state)
+      let prev = parts[1].split(',')
+      let job = parts[2]
 
-      followPtrs(0, ptrs, new Set(), (err, bots) => {
+      followPointers(0, prev, new Set(), (err, bots) => {
         if (err) {
-          state = WAIT_ROLLCALL
-          console.error('unable to start job', parts[2], 'because error:', err)
+          state = states.WAIT_ROLL
+          console.error('unable to start job', job, 'because error:', err)
         } else if (bots.has(id) >= 0) {
-          state = DO_JOB
-          console.log('do job w/', bots)
+          state = states.DO_JOB
+          let archives = Array.from(bots).map((key) => peers[key])
+          console.log('do job w/', bots, archives)
         } else {
           console.log('skip job w/', bots)
         }
-        next(idx + 1, cb)
+        cb(null, state)
       })
     } else if (/^!done\b/.test(text)) {
-      if (state === DO_JOB && id === msg.key) {
-        state = WAIT_ROLLCALL
+      if (state === states.DO_JOB && id === msg.key) {
+        state = states.WAIT_ROLL
       }
-      next(idx + 1, cb)
+      cb(null, state)
     } else {
-      next(idx + 1, cb)
+      cb(null, state)
     }
   }
 
   return { next }
 }
-
-SwarmState(inboxE.key, inboxE.inbox)
-  .next(0, (err, state) => {
-    console.error('error', err)
-    console.log(state)
-
-    kv.get('head', (err, ids) => {
-      console.error('error', err)
-      console.log('head ->', ids)
-    })
-  })
