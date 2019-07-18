d5eebaf5b6d4d6c61796f31074958567c2690a0e
rhodey <notrhodey@protonmail.com>
2019-07-18 11:34:20 -1000

    downloading job dat

--- nonce.js
+++ nonce.js
@@ -1,14 +1,16 @@
-const crypto = require('crypto')
-const level = require('memdb')
-const umkv = require('unordered-materialized-kv')
-const BotState = require('./state.js').BotState
-const states = require('./state.js').states
-
-const path       = require('path')
-const minimist   = require('minimist')
-const mkdirp     = require('mkdirp')
-const hyperdrive = require('hyperdrive')
-const Cabal      = require('cabal-core')
+const minimist      = require('minimist')
+const exec          = require('child_process').exec
+const mkdirp        = require('mkdirp')
+const crypto        = require('crypto')
+const path          = require('path')
+const level         = require('memdb')
+const umkv          = require('unordered-materialized-kv')
+const hyperdrive    = require('hyperdrive')
+const Cabal         = require('cabal-core')
+const Swarm         = require('discovery-swarm')
+const swarmDefaults = require('dat-swarm-defaults')
+const BotState      = require('./state.js').BotState
+const states        = require('./state.js').states
 
 function error (err) {
   if (err) {
@@ -28,7 +30,7 @@ mkdirp.sync(argv.datadir)
 let addr = argv._[0].replace(/^cabal:\/*/,'')
 let db = level()
 let cabal = Cabal(path.join(argv.datadir, 'cabal'), addr, { db })
-let drive = hyperdrive(path.join(argv.datadir, 'dat'))
+let datout = hyperdrive(path.join(argv.datadir, 'datout'))
 let botid = undefined
 
 function fetchMail(channel, cb) {
@@ -68,25 +70,39 @@ function work() {
         case states.ACK_ROLL:
           kv.get('head', (err, ids) => {
             let nonce = crypto.randomBytes(2).toString('hex')
-            let uri = 'dat://' + drive.key.toString('hex')
+            let uri = 'dat://' + datout.key.toString('hex')
             let ack = '!ok ' + nonce + ' ' + ids.join(',') + ' ' + uri
             publish(ack)
+            setTimeout(work, 2500)
           })
           break;
 
         case states.DO_JOB:
           let job = botstate.job()
-          console.log('do job ->', job)
+          let key = job.uri.replace(/^dat:\/*/,'')
+          let pathin = path.join(argv.datadir, 'datin')
+          exec('rm -rf ' + pathin, (err, stdout, stderr) => {
+            if (err) return error(err)
+            exec('dat clone ' + job.uri + ' ' + pathin, (err, stdout, stderr) => {
+              if (err) return error(err)
+              else {
+                console.log(stdout)
+                setTimeout(work, 2500)
+              }
+            })
+          })
           break;
+
+        default:
+          setTimeout(work, 2500)
       }
-      setTimeout(work, 2500)
     })
   }))
 }
 
 cabal.swarm(error)
-drive.once('error', error)
-drive.once('ready', () => {
+datout.once('error', error)
+datout.once('ready', () => {
   cabal.getLocalKey((err, key) => {
     if (err) { error(err) }
     else {
