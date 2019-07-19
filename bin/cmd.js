02bde8eceed869a0687e95df6abae5a416c035e8
rhodey <notrhodey@protonmail.com>
2019-07-18 15:10:54 -1000

    cleanup

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,80 +1,132 @@
-#!/usr/bin/env node
-var minimist = require('minimist')
+const minimist      = require('minimist')
+const fs            = require('fs')
+const proc          = require('child_process')
+const mkdirp        = require('mkdirp')
+const crypto        = require('crypto')
+const path          = require('path')
+const level         = require('memdb')
+const umkv          = require('unordered-materialized-kv')
+const hyperdrive    = require('hyperdrive')
+const Cabal         = require('cabal-core')
+const Swarm         = require('discovery-swarm')
+const swarmDefaults = require('dat-swarm-defaults')
+const BotState      = require('../index.js').BotState
+const states        = require('../index.js').states
+
+function error (err) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
 var argv = minimist(process.argv.slice(2), {
-  alias: { d: 'datadir', s: 'swarm' }
+  alias: { d : 'datadir', c : 'channel' },
+  default: { datadir : '.datadir', channel : 'bots' },
+  string: [ '_' ]
 })
 
-function open () {
-  var swarmhead = require('../')
-  var levelup = require('levelup')
-  var leveldown = require('leveldown')
-  var path = require('path')
-  var mkdirp = require('mkdirp')
-  mkdirp.sync(path.join(argv.datadir,'db'))
-  return swarmhead({
-    hypercore: require('hypercore'),
-    swarm: require('discovery-swarm'),
-    exec: require('child_process').exec,
-    storage: path.join(argv.datadir,'store'),
-    db: levelup(leveldown(path.join(argv.datadir,'db')))
+mkdirp.sync(argv.datadir)
+
+let addr = argv._[0].replace(/^cabal:\/*/,'')
+let db = level()
+let cabal = Cabal(path.join(argv.datadir, 'cabal'), addr, { db })
+let datout = hyperdrive(path.join(argv.datadir, 'datout'))
+let datkey = undefined
+let botkey = undefined
+
+function fetchMail(channel, cb) {
+  let arr = []
+  cabal.messages.read(channel, { limit : 100 })
+    .on('data', (msg) => arr.push(msg))
+    .once('end', () => cb(null, arr.reverse()))
+}
+
+function readAll(state, mail, idx, cb) {
+  if (idx >= mail.length) return cb(null, state)
+  state.next(mail[idx], (err, s) => {
+    if (err && err.notFound) console.error(err)
+    if (err && !err.notFound) return cb(err)
+    else readAll(state, mail, idx + 1, cb)
   })
 }
 
-if (argv._[0] === 'create') {
-  var sh = open()
-  sh.create(function (err, key) {
-    console.log(key.toString('hex'))
+function publish (text) {
+  cabal.publish({
+    type: 'chat/text',
+    content: { channel: 'bots', text }
   })
-} else if (argv._[0] === 'deploy') {
-  var sh = open()
-  var cmd = argv._.slice(1).join(' ')
-  sh.deploy(cmd, function (err, job) {
+}
+
+function work() {
+  let bdb = level()
+  let kv = umkv(bdb)
+  let botstate = BotState(botkey, bdb, kv)
+
+  fetchMail('bots', ((err, mail) => {
     if (err) return error(err)
-    else console.log(job)
-  })
-} else if (argv._[0] === 'join') {
-  var sh = open()
-  sh.join(argv._[1] && String(argv._[1]), function (err, sw) {
-    if (err) return error(err)
-    sw.on('connection', function (stream, info) {
-      console.log('connection',
-        `${info.host}:${info.port} ${info.id.toString('hex')}`)
+    readAll(botstate, mail, 0, (err, bs) => {
+      if (err) return error(err)
+      console.log('end state ->', botstate.state())
+
+      switch (botstate.state()) {
+        case states.ACK_ROLL:
+          kv.get('head', (err, ids) => {
+            let nonce = crypto.randomBytes(2).toString('hex')
+            let ack = `!ok ${nonce} ${ids.join(',')} dat://${datkey}`
+            publish(ack)
+            setTimeout(work, 2500)
+          })
+          break;
+
+        case states.DO_JOB:
+          let job = botstate.job()
+          let pathin = path.join(argv.datadir, 'datin')
+          proc.exec(`rm -rf ${pathin}`, (err, stdout, stderr) => {
+            if (err) return error(err)
+            proc.exec(`dat clone ${job.uri} ${pathin}`, (err, stdout, stderr) => {
+              if (err) return error(err)
+              proc.exec('npm install', { cwd : pathin }, (err) => {
+                if (err) return error(err)
+                let pathout = path.resolve(path.join(argv.datadir, 'datout'))
+                let config = Object.assign({ }, job, { hyperdrive : pathout })
+                let configFile = path.join(pathin, 'config.json')
+                fs.writeFileSync(configFile, JSON.stringify(config))
+
+                let child = proc.spawn('node', ['index.js'], { cwd : pathin })
+                child.stdout.on('data', (data) => publish('!stdout> ' + data.toString().trim()))
+                child.stderr.on('data', (data) => publish('!stderr> ' + data.toString().trim()))
+                child.once('close', (code) => {
+                  if (code === 0) {
+                    publish(`!done ${job.uri}`)
+                  } else {
+                    publish(`!error ${job.uri}`)
+                  }
+                  setTimeout(work, 2500)
+                })
+              })
+            })
+          })
+          break;
+
+        default:
+          setTimeout(work, 2500)
+      }
     })
-  })
-} else if (argv._[0] === 'work') {
-  var sh = open()
-  sh.work(argv._[1], function (err, w) {
-    if (err) return error(err)
-    w.on('job', function (job) {
-      console.error(`# job started: ${job.id}`)
-      job.process.stdout.pipe(process.stdout, { end: false })
-      job.process.stderr.pipe(process.stderr, { end: false })
-      job.process.on('exit', function (code) {
-        console.error(`# job exited: ${job.id} code: ${code}`)
-      })
-      job.swarm.on('connection', function (stream, info) {
-        console.log('connection',
-          `${info.host}:${info.port} ${info.id.toString('hex')}`)
-      })
-    })
-  })
-} else if (argv._[0] === 'jobs') {
-  var sh = open()
-  sh.jobs(function (err, jobs) {
-    if (err) return error(err)
-    console.log(jobs)
-  })
-} else if (argv._[0] === 'logs') {
-  var through = require('through2')
-  var sh = open()
-  sh.logs(argv._[1]).pipe(through.obj(function (row, enc, next) {
-    var w = row.worker.slice(0,8)
-    console.log(`[${w}@${row.time}] ${row.data.toString()}`)
-    next()
   }))
 }
 
-function error (err) {
-  console.error(err)
-  process.exit(1)
-}
+cabal.swarm(error)
+datout.once('error', error)
+datout.once('ready', () => {
+  datkey = datout.key.toString('hex')
+  datout.close()
+  cabal.getLocalKey((err, key) => {
+    if (err) { error(err) }
+    else {
+      console.log(key)
+      botkey = key
+      work()
+    }
+  })
+})
--- index.js
+++ index.js
@@ -1,278 +1,134 @@
-var multifeed = require('multifeed')
-var kcore = require('kappa-core')
-var randombytes = require('randombytes')
-var pump = require('pump')
-var split = require('split2')
-var through = require('through2')
-var EventEmitter = require('events').EventEmitter
-var duplexify = require('duplexify')
-var collect = require('collect-stream')
-
-var JOB = 'j!'
-var OUTPUT = 'o!'
-var STDOUT = '1!'
-var STDERR = '2!'
-var EXIT = '3!'
-
-var types = { 1: 'stdout', 2: 'stderr' }
-
-module.exports = Swarmhead
-
-function Swarmhead (opts) {
-  var self = this
-  if (!(this instanceof Swarmhead)) return new Swarmhead(opts)
-  this.hypercore = opts.hypercore
-  this.swarm = opts.swarm
-  this.storage = opts.storage
-  this.exec = opts.exec
-  this.multi = multifeed(this.hypercore, this.storage,
-    { valueEncoding: 'json' })
-  this.kcore = kcore(this.storage, { multifeed: this.multi })
-  this.db = opts.db
-  this.kcore.use('logs', 1, {
-    api: {
-      list: function (core, id) {
-        var d = duplexify.obj()
-        core.ready(function () {
-          var out = through.obj(function (row, enc, next) {
-            var parts = row.key.toString().split('!')
-            var type = types[parts[3]]
-            if (type === 'stdout' || type === 'stderr') {
-              next(null, {
-                type: type,
-                worker: parts[2],
-                time: parts[4],
-                data: row.value
-              })
-            } else if (type === 'exit') {
-              next(null, {
-                type: type,
-                code: Number(row.value.toString())
-              })
-            } else next()
-          })
-          d.setReadable(out)
-          pump(self.db.createReadStream({
-            gt: OUTPUT + id + '!',
-            lt: OUTPUT + id + '!\uffff',
-            keyEncoding: 'utf8',
-            valueEncoding: 'json'
-          }), out)
-        })
-        return d
-      }
-    },
-    map: function (msgs, next) {
-      var batch = []
-      msgs.forEach(function (msg) {
-        if (msg.value && msg.value.type === 'stderr') {
-          batch.push({
-            type: 'put',
-            key: OUTPUT + msg.value.job + '!' + msg.value.worker
-              + '!' + STDERR + msg.value.time,
-            value: msg.value.data
-          })
-        } else if (msg.value && msg.value.type === 'stdout') {
-          batch.push({
-            type: 'put',
-            key: OUTPUT + msg.value.job + '!' + msg.value.worker
-              + '!' + STDOUT + msg.value.time,
-            value: msg.value.data
-          })
-        } else if (msg.value && msg.value.type === 'exit') {
-          batch.push({
-            type: 'put',
-            key: OUTPUT + msg.value.job + '!' + msg.value.worker
-              + '!' + EXIT + msg.value.time,
-            value: msg.value.code
-          })
-        }
-      })
-      self.db.batch(batch, { valueEncoding: 'json' }, next)
-    }
-  })
-  self.kcore.use('jobs', 1, {
-    api: {
-      list: function () {
-        var d = duplexify.obj()
-        this.ready(function () {
-          var out = through.obj(function (row, enc, next) {
-            next(null, row.key.toString().slice(JOB.length))
-          })
-          pump(self.db.createReadStream({
-            gt: JOB,
-            lt: JOB + '\uffff',
-            keyEncoding: 'utf8',
-            valueEncoding: 'json'
-          }), out)
-          d.setReadable(out)
-        })
-        return d
-      }
-    },
-    map: function (msgs, next) {
-      var batch = []
-      msgs.forEach(function (msg) {
-        if (msg.value && msg.value.type === 'deploy') {
-          batch.push({
-            type: 'put',
-            key: JOB + msg.value.id,
-            value: 0
-          })
-        }
-      })
-      self.db.batch(batch, { valueEncoding: 'json' }, next)
-    }
-  })
+const states = {
+  WAIT_ROLL: 0,
+  ACK_ROLL : 1,
+  WAIT_JOB : 2,
+  DO_JOB : 3,
 }
 
-Swarmhead.prototype.create = function (cb) {
-  var self = this
-  if (!cb) cb = noop
-  self.kcore.feed('node', function (err, feed) {
-    if (err) return cb(err)
-    cb(null, feed.key)
-  })
+module.exports = {
+  states, BotState
 }
 
-Swarmhead.prototype.deploy = function (cmd, cb) {
-  if (!cb) cb = noop
-  this.kcore.feed('jobs', function (err, feed) {
-    if (err) return cb(err)
-    var id = randombytes(8).toString('hex')
-    feed.append({
-      type: 'deploy',
-      command: cmd,
-      id
-    }, onappend)
-    function onappend (err, seq) {
+function BotState(id, db, kv) {
+  let nonces = { }
+  let peers = { }
+  let state = states.WAIT_ROLL
+  let job = undefined
+
+  function clearDb(cb) {
+    db.createKeyStream()
+      .on('data', (key) => db.del(key))
+      .once('end', cb)
+  }
+
+  function filter(msg) {
+    if (!msg.value) return
+    if (!msg.value.content) return
+    if (msg.value.content.channel !== 'bots') return
+    if (typeof msg.value.content.text !== 'string') return
+    return true
+  }
+
+  function followPointer(ptr, bots, cb) {
+    let val = nonces[ptr]
+    if (val === undefined) {
+      let err = new Error(`message ${ptr} not found.`)
+      err.notFound = true
+      cb(err)
+    } else if (val === 0) {
+      cb(null)
+    } else {
+      let botid = val.id
+      bots.add(botid)
+      followPointers(0, val.prev.split(','), bots, cb)
+    }
+  }
+
+  function followPointers(idx, ptrs, bots, cb) {
+    if (idx >= ptrs.length) { return cb(null, bots) }
+    let ptr = ptrs[idx]
+
+    followPointer(ptr, bots, (err) => {
       if (err) cb(err)
-      else cb(null, id)
-    }
-  })
-}
-
-Swarmhead.prototype.join = function (id, cb) {
-  if (!cb) cb = noop
-  var self = this
-  self.kcore.feed('node', function (err, feed) {
-    if (err) return cb(err)
-    var sw = self.swarm({ id: feed.key })
-    sw.listen()
-    sw.join(id ? toBuf(id) : feed.key)
-    sw.on('connection', function (stream, info) {
-      var r = self.multi.replicate({ live: true })
-      pump(r, stream, r)
+      else followPointers(idx + 1, ptrs, bots, cb)
     })
-    cb(null, sw)
-  })
-}
-
-Swarmhead.prototype.work = function (id, cb) {
-  var self = this
-  var w = new EventEmitter
-  self.join(id, function (err, sw) {
-    if (err) return cb(err)
-    w.swarm = sw
-    self.kcore.use('work', 1, {
-      api: {},
-      map: map
-    })
-    cb(null, w)
-  })
-  function map (msgs, next) {
-    ;(function advance (i) {
-      if (i >= msgs.length) return next()
-      var msg = msgs[i]
-      //if (msg.key !== id || !msg.value
-      if (!msg || !msg.value
-      || msg.value.type !== 'deploy'
-      || !msg.value.id
-      || typeof msg.value.command !== 'string'
-      ) return advance(i+1)
-      run(msg, function (err) {
-        if (err) next(err)
-        else advance(i+1)
-      })
-    })(0)
   }
-  function run (msg, cb) {
-    var jobId = msg.value.id
-    self.kcore.feed('result/' + jobId, function (err, feed) {
-      if (err) return cb(err)
-      var ps = self.exec(msg.value.command)
-      var job = new EventEmitter
-      job.id = jobId
-      job.process = ps
-      job.swarm = w.swarm
-      w.emit('job', job)
-      ps.on('error', function (err) {
-        job.emit('error', err)
-        cb(err)
-      })
-      var pending = 3
-      ps.on('exit', function (code) {
-        feed.append({
-          type: 'exit',
-          job: jobId,
-          worker: feed.key.toString('hex'),
-          time: Date.now(),
-          code: code
-        })
-        job.emit('exit', code)
-        done()
-      })
-      ps.stderr.pipe(split()).pipe(through(function (buf, enc, next) {
-        feed.append({
-          type: 'stderr',
-          job: jobId,
-          worker: feed.key.toString('hex'),
-          time: Date.now(),
-          data: buf.toString()
-        })
-        next()
-      }, done))
-      ps.stdout.pipe(split()).pipe(through(function (buf, enc, next) {
-        feed.append({
-          type: 'stdout',
-          job: jobId,
-          worker: feed.key.toString('hex'),
-          time: Date.now(),
-          data: buf.toString()
-        })
-        next()
-      }, done))
-      function done () {
-        if (--pending === 0) cb()
+
+  function next(msg, cb) {
+    let text = msg.value.content.text
+
+    if (/^!rollcall\b/.test(text)) {
+      let parts = text.split(' ')
+      if (parts.length != 2) return cb(null, state)
+      if (state !== states.DO_JOB) {
+        state = states.ACK_ROLL
       }
-    })
+
+      let nonce = parts[1]
+      nonces[nonce] = 0
+      job = undefined
+
+      clearDb(() => {
+        let doc = { id : nonce, key : 'head', links : [] }
+        kv.batch([doc], (err) => {
+          if (err) cb(err)
+          else cb(null, state)
+        })
+      })
+    } else if (/^!ok\b/.test(text)) {
+      let parts = text.split(' ')
+      if (parts.length != 4) return cb(null, state)
+      if (state === states.ACK_ROLL && id === msg.key) {
+        state = states.WAIT_JOB
+      }
+
+      let nonce = parts[1]
+      let prev = parts[2]
+      let archive = parts[3]
+
+      nonces[nonce] = { id : msg.key, prev }
+      peers[msg.key] = archive
+
+      let doc = { id : nonce, key : 'head', links : prev.split(',') }
+      kv.batch([doc], (err) => {
+        if (err) cb(err)
+        else cb(null, state)
+      })
+    } else if (/^!job\b/.test(text)) {
+      let parts = text.split(' ')
+      if (parts.length != 3) cb(null, state)
+      let prev = parts[1].split(',')
+
+      if (state === states.WAIT_JOB) {
+        followPointers(0, prev, new Set(), (err, bots) => {
+          if (err) {
+            state = states.WAIT_ROLL
+            return cb(err)
+          } else if (!bots.has(id)) {
+            state = states.WAIT_ROLL
+          } else {
+            state = states.DO_JOB
+            job = { uri : parts[2], peers : { } }
+            bots.forEach((key) => job.peers[key] = peers[key])
+          }
+          cb(null, state)
+        })
+      } else if (state !== state.DO_JOB) {
+        state = states.WAIT_ROLL
+        cb(null, state)
+      } else {
+        cb(null, state)
+      }
+    } else if (/^!done\b/.test(text) || /^!error\b/.test(text)) {
+      if (state === states.DO_JOB && id === msg.key) {
+        state = states.WAIT_ROLL
+        job = undefined
+      }
+      cb(null, state)
+    } else {
+      cb(null, state)
+    }
   }
-}
 
-Swarmhead.prototype.jobs = function (cb) {
-  if (!cb) cb = noop
-  var self = this
-  var d = duplexify.obj()
-  self.kcore.ready('jobs', function () {
-    d.setReadable(self.kcore.api.jobs.list())
-  })
-  if (cb) collect(d, cb)
-  return d
-}
-
-Swarmhead.prototype.logs = function (id, cb) {
-  if (!cb) cb = noop
-  var self = this
-  var d = duplexify.obj()
-  self.kcore.ready('logs', function () {
-    d.setReadable(self.kcore.api.logs.list(id))
-  })
-  if (cb) collect(d, cb)
-  return d
-}
-
-function noop () {}
-
-function toBuf (x) {
-  return typeof x === 'string' ? Buffer.from(x, 'hex') : x
+  return { next, state : () => state, job : () => job }
 }
